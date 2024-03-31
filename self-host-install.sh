32a2886e12e31b6b3da26dcdc389b78daae1b083
Gilad Neiger <gilad.neiger@develeap.com>
2024-03-31 21:05:13 +0200

    Remove unecessary docker image layers for whisper

--- self-host-install.sh
+++ self-host-install.sh
@@ -4,5 +4,4 @@ kubectl cluster-info --context kind-kind
 kind load docker-image whisperqday:qday
 kind load docker-image streamlitqday:qday
 helm install whisper-streamlit-helm whisper-streamlit-helm
-sleep 15
 kubectl port-forward service/streamlit 8501:8501
