4c911c69c35955c419d381793e2081adbf06663a
Gilad Neiger <gilad.neiger@develeap.com>
2024-04-04 08:33:45 +0300

    Update whisper bin to be downloaded directly to models directory inside project

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 docker build -t whisperqday:qday whisper-model-service
 docker build -t streamlitqday:qday streamlit
-wget --no-config --quiet --show-progress -O ggml-small.bin https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-small.bin
+wget --no-config --quiet --show-progress -O models/ggml-small.bin https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-small.bin
